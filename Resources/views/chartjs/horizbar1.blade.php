22287dfcd0c79c9a9508ddaee912477a28dc4442
Malebestia <malebestia@yahoo.it>
2022-05-22 17:21:45 +0200

    .

--- Resources/views/chartjs/horizbar1.blade.php
+++ Resources/views/chartjs/horizbar1.blade.php
@@ -10,6 +10,7 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
+    //
     const labels = [
         'January',
         'February',
@@ -19,24 +20,42 @@
         'June',
     ];
 
+
     const data = {
-        labels: labels, // valori sull'asse delle x
+        labels: labels,
         datasets: [{
-            label: 'My First dataset', // legenda(?) supra il grafico
-            // colori delle barre, 
-            // può essere anche un array con colori diversi, ogniuna per ogni barra
-            backgroundColor: 'rgb(255, 99, 132)',
-            //borderColor: 'rgb(100, 100, 100)', // colore bordo delle barre
-            borderColor: '#fff',
-            borderWith: 20,
-            data: [0, 10, 5, 2, 20, 30, 45], // valori sull'asse y
+            axis: 'y',
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
         }]
     };
 
     const config = {
         type: 'bar',
-        data: data,
-        options: {}
+        data,
+        options: {
+            indexAxis: 'y',
+        }
     };
 
     const myChart = new Chart(
--- Services/ChartEngines/ChartJsEngineTraits/LineTrait.php
+++ Services/ChartEngines/ChartJsEngineTraits/LineTrait.php
@@ -5,4 +5,19 @@ declare(strict_types=1);
 namespace Modules\Chart\Services\ChartEngines\ChartJsEngineTraits;
 
 trait LineTrait {
+    public function line1() {
+        $view = 'chart::chartjs.'.__FUNCTION__;
+        $view_params = [
+            'view' => $view,
+            'filename' => 'prova123',
+        ];
+
+        //dddx($view_params);
+
+        $out = view()->make($view, $view_params);
+        $html = $out->render();
+        exit($html);
+
+        return $this;
+    }
 }
