259c60aba6cddba16ff7347f62e14c119629d042
Malebestia <malebestia@yahoo.it>
2022-05-23 12:16:38 +0200

    .

--- Services/ChartEngines/ChartJsEngineTraits/BarTrait.php
+++ Services/ChartEngines/ChartJsEngineTraits/BarTrait.php
@@ -6,10 +6,15 @@ namespace Modules\Chart\Services\ChartEngines\ChartJsEngineTraits;
 
 trait BarTrait {
     public function bar2(): self {
+        $labels = $this->data->pluck('label')->all();
+        $data = $this->data->pluck('value')->all();
+
         $view = 'chart::chartjs.'.__FUNCTION__;
         $view_params = [
             'view' => $view,
             'filename' => 'prova123',
+            'labels' => $labels,
+            'data' => $data,
         ];
 
         //dddx($view_params);
