b4232aafc03ed340125f91c5644cbf2632c1218b
marco76tv <marco.sottana@gmail.com>
2022-05-23 11:13:54 +0200

    up

--- Services/ChartEngines/ChartJsEngineTraits/HorizbarTrait.php
+++ Services/ChartEngines/ChartJsEngineTraits/HorizbarTrait.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @link https://www.larashout.com/laravel-collection-using-tojson-method
+ * @link https://codepen.io/k-sav/pen/PXxywK
+ */
 
 declare(strict_types=1);
 
@@ -9,10 +13,18 @@ trait HorizbarTrait {
      * Undocumented function.
      */
     public function horizbar1(): self {
+        //$view = 'chart::'.(inAdmin()?'admin.':'').'chartjs.'.__FUNCTION__;
+
+        $labels=$this->data->pluck('label')->all();
+        $data=$this->data->pluck('value')->all();
+        //dddx(['data'=>$this->data]);
         $view = 'chart::chartjs.'.__FUNCTION__;
+        //$view.='.1';
         $view_params = [
             'view' => $view,
             'filename' => 'prova123',
+            'labels'=>$labels,
+            'data'=>$data,
         ];
 
         //dddx($view_params);
