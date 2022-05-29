1b5c0b4360ed47330419e438fd37095b8c0ca327
marco76tv <marco.sottana@gmail.com>
2022-05-29 16:39:07 +0100

    .

--- Services/ChartEngines/JpgraphEngineTraits/HorizbarTrait.php
+++ Services/ChartEngines/JpgraphEngineTraits/HorizbarTrait.php
@@ -45,7 +45,7 @@ trait HorizbarTrait {
         }
 
         if (isset($this->vars['tot'])) {
-            $subtitle = 'Totale Rispondenti '.$this->vars['tot'].' - ('.$mandatory.')';
+            $subtitle = 'Totale Rispondenti '.$this->vars['tot']; //.' - ('.$mandatory.')';
             if ('Y' != $this->vars['mandatory']) {
                 if (isset($this->vars['tot_nulled'])) {
                     $subtitle .= ' Non rispondenti '.$this->vars['tot_nulled'];
