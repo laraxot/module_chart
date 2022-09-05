eff8566f2255549391e2595788d25922a6e4bc04
marco76tv <marco.sottana@gmail.com>
2022-09-05 13:14:24 +0200

    up

---
+++ Services/ChartEngines/googleChartEngine.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * https://it.scribd.com/document/27111189/Jpgraph-Manual.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Chart\Services\ChartEngines;
+
+/**
+ * ---.
+ * */
+class googleChartEngine
+{
+}
