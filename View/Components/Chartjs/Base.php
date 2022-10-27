9939c6167525de5c8138bb04d58f5960001896e9
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-27 13:21:24 +0000

    .

--- View/Components/Chartjs/Base.php
+++ View/Components/Chartjs/Base.php
@@ -12,14 +12,20 @@ use Illuminate\View\Component;
  */
 class Base extends Component {
     public string $type;
-
+    public array $labels;
+    public array $data;
+    public string $chartid;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct(string $type) {
+    public function __construct(string $chartid, string $type, array $labels, array $data) {
         $this->type = $type;
+        $this->labels = $labels;
+        $this->data = $data;
+        $this->chartid=$chartid;
+        //dddx([$type, $labels, $data]);
     }
 
     /**
@@ -33,6 +39,9 @@ class Base extends Component {
 
         $view_params = [
             'view' => $view,
+            'labels'=>$this->labels,
+            'data'=>$this->data,
+            'chartid'=>$this->chartid
         ];
 
         return view()->make($view, $view_params);
