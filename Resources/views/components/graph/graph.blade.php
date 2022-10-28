fffc12bd106b29476589e483191b6be80ec10743
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-28 12:40:11 +0000

    up

--- Resources/views/components/graph/graph.blade.php
+++ Resources/views/components/graph/graph.blade.php
@@ -2,31 +2,45 @@
     <canvas id="{{ $graph_id }}" width="400" height="400"></canvas>
 </div>
 @push('scripts')
-<script>
-const ctx = document.getElementById('{{ $graph_id }}').getContext('2d');
+    <script>
+        const ctx = document.getElementById('{{ $graph_id }}').getContext('2d');
 
-async function fetchData(){
-    const url="{{ $url }}";
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.log(data);
-    return data;
-}
+        async function fetchData() {
+            const url = "{{ $url }}";
 
-fetchData().then(data => {
-    //myChart.config.data=data;
-    //myChart.update();
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+
+
+            const options = {
+                method: "GET",
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Accept': "application/json",
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'), 
                 }
             }
+            const test = await fetch('/api/intellinet', options);
+
+            const response = await fetch(url, options);
+            const data = await response.json();
+            console.log(data);
+            return data;
         }
-    });
-});
-</script>
+
+        fetchData().then(data => {
+            //myChart.config.data=data;
+            //myChart.update();
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+                data: data,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+    </script>
 @endpush
