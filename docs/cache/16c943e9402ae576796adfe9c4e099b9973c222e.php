b615f5935797567f23f5c6aec2617f7aa0e0f69e
marco76tv <marco.sottana@gmail.com>
2022-12-13 11:04:01 +0100

    .

--- docs/cache/16c943e9402ae576796adfe9c4e099b9973c222e.php
+++ docs/cache/16c943e9402ae576796adfe9c4e099b9973c222e.php
@@ -43,8 +43,8 @@
         <header class="flex items-center shadow bg-white border-b h-24 mb-8 py-4" role="banner">
             <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center">
-                    <a href="/" title="<?php echo e($page->siteName); ?> home" class="inline-flex items-center">
-                        <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="<?php echo e($page->siteName); ?> logo" />
+                    <a href="<?php echo e(url('/')); ?>" title="<?php echo e($page->siteName); ?> home" class="inline-flex items-center">
+                        <img class="h-8 md:h-10 mr-3" src="<?php echo e(url('/assets/img/logo.svg')); ?>" alt="<?php echo e($page->siteName); ?> logo" />
 
                         <h1 class="text-lg md:text-2xl text-blue-900 font-semibold hover:text-blue-600 my-0 pr-4"><?php echo e($page->siteName); ?></h1>
                     </a>
