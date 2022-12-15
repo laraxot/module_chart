6bc08ed4c864e1c4c05ea16a9b24732b4ae35604
marco xot <marco.sottana@gmail.com>
2022-12-15 12:43:00 +0100

    up

--- View/Components/Chartjs/Base.php
+++ View/Components/Chartjs/Base.php
@@ -12,6 +12,7 @@ use Illuminate\View\Component;
  */
 class Base extends Component {
     public string $type;
+    public string $title;
     public array $labels;
     public array $data;
     public string $chartid;
