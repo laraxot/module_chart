7641eccb839200d74ad005ee95c3a0b443e3c7de
Retype GitHub Action <retype@object.net>
2023-01-24 19:25:23 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/graph/graph.blade.php
+++ resources/views/components/graph/graph.blade.php
@@ -27,14 +27,52 @@
             }
 
             load = function() {
-                this.fetchData().then(data => {
+                this.fetchData().then(config => {
+
+                    console.log(config.options);
+                    // const labels = ['marzo', 'aprile', 'maggio'];
+                    const data = {
+                        labels: config[0].labels,
+                        // datasets: [{
+                        //     axis: 'y',
+                        //     label: 'My First Dataset',
+                        //     data: [65, 59, 80, 81, 56, 55, 40],
+                        //     fill: false,
+                        //     backgroundColor: [
+                        //         'rgba(255, 99, 132, 0.2)',
+                        //         'rgba(255, 159, 64, 0.2)',
+                        //         'rgba(255, 205, 86, 0.2)',
+                        //         'rgba(75, 192, 192, 0.2)',
+                        //         'rgba(54, 162, 235, 0.2)',
+                        //         'rgba(153, 102, 255, 0.2)',
+                        //         'rgba(201, 203, 207, 0.2)'
+                        //     ],
+                        //     borderColor: [
+                        //         'rgb(255, 99, 132)',
+                        //         'rgb(255, 159, 64)',
+                        //         'rgb(255, 205, 86)',
+                        //         'rgb(75, 192, 192)',
+                        //         'rgb(54, 162, 235)',
+                        //         'rgb(153, 102, 255)',
+                        //         'rgb(201, 203, 207)'
+                        //     ],
+                        //     borderWidth: 1
+                        // }]
+                        datasets: config[0].datasets
+
+
+                    };
                     const myChart = new Chart(this.ctx, {
-                        type: data.type,
-                        data: data,
-                        options: data.options,
+                        type: config.type,
+                        data,
+                        // options: {
+                        //     indexAxis: 'y',
+                        // }
+                        options: config.options
                     });
                 });
             }
+
         }
     </script>
 @endpushonce
