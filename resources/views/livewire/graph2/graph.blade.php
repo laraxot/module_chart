01beb0cbe5794bae15804a28b10646ec14677423
Retype GitHub Action <retype@object.net>
2023-02-20 09:41:21 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/livewire/graph2/graph.blade.php
+++ resources/views/livewire/graph2/graph.blade.php
@@ -5,14 +5,17 @@
         //  $url = url_queries(['api_token' => \Auth::user()->api_token], $url);
     @endphp
 </div>
+
+
 @pushonce('scripts')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         class Graph {
-            constructor(id) {
+            constructor(id, config) {
 
                 this.ctx = document.getElementById(id).getContext('2d');
                 this.url = document.getElementById(id).getAttribute('url');
+                this.config = config;
                 // this.type = document.getElementById(id).getAttribute('type');
 
                 console.log(id, this);
@@ -23,7 +26,7 @@
 
             load = function() {
                     // TODO: se il config è un array con più config vanno messi uno affianco all'altro
-                    const myChart = new Chart(this.ctx, {!! html_entity_decode($config) !!}
+                    const myChart = new Chart(this.ctx,  this.config
                     );
             }
 
@@ -31,8 +34,13 @@
     </script>
 @endpushonce
 
+
 @push('scripts')
+    @if($load)
     <script>
-        new Graph('{{ $graph_id }}');
+        new Graph('{{ $graph_id }}', '{!! html_entity_decode($config) !!}');
     </script>
+    @endif
 @endpush
+
+
