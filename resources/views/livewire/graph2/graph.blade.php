3fe096a41c05fcf5d174b77cad739c70a43832ad
Retype GitHub Action <retype@object.net>
2023-02-21 09:52:43 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/livewire/graph2/graph.blade.php
+++ resources/views/livewire/graph2/graph.blade.php
@@ -1,46 +1,31 @@
-<div wire:init="loadGraph">
-    <canvas id="{{ $graph_id }}" width="400" height="400" url="{{ $url }}"
-        type="{{ $type }}"></canvas>
+<div wire:init="loadGraph" >
+    <canvas id="{{ $graph_id }}" width="400" height="400"
+        type="{{ $type }}" ></canvas>
     @php
         //  $url = url_queries(['api_token' => \Auth::user()->api_token], $url);
     @endphp
 </div>
 
-
 @pushonce('scripts')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        class Graph {
-            constructor(id, config) {
 
-                this.ctx = document.getElementById(id).getContext('2d');
-                this.url = document.getElementById(id).getAttribute('url');
-                this.config = config;
-                // this.type = document.getElementById(id).getAttribute('type');
-
-                console.log(id, this);
-
-                this.load();
-            }
-
-
-            load = function() {
-                    // TODO: se il config è un array con più config vanno messi uno affianco all'altro
-                    const myChart = new Chart(this.ctx,  this.config
-                    );
-            }
-
-        }
-    </script>
 @endpushonce
 
-
 @push('scripts')
-    @if($load)
+
     <script>
-        new Graph('{{ $graph_id }}', '{!! html_entity_decode($config) !!}');
+        var chart{{$graph_id}} = new Chart(
+            document.getElementById({{ $graph_id }}), @json($config)
+        );
+        Livewire.on('updateChart{{ $graph_id }}', data => {
+            // chart.data =data['data'];
+            // chart.options =data['options'];
+            // chart.config.type =data['type'];
+            // chart.update();
+            chart{{$graph_id}}.destroy()
+            chart{{$graph_id}} = new Chart(document.getElementById({{ $graph_id }}), data)
+        });
     </script>
-    @endif
 @endpush
 
 
