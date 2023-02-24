9d2941c844eeefb32cbae3978d361e71ad9c09e6
Retype GitHub Action <retype@object.net>
2023-02-24 15:05:18 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/livewire/graph2/graph.blade.php
+++ resources/views/livewire/graph2/graph.blade.php
@@ -1,6 +1,8 @@
 <div wire:init="loadGraph" >
-    <canvas id="{{ $graph_id }}" width="400" height="400"
-        type="{{ $type }}" ></canvas>
+
+    @if($readyToLoadGraph)
+        @livewire('graphdetails', ['url' => $url, 'id'=>$graph_id])
+    @endif
     @php
         //  $url = url_queries(['api_token' => \Auth::user()->api_token], $url);
     @endphp
@@ -8,24 +10,13 @@
 
 @pushonce('scripts')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
+    <script >
+        Chart.register(ChartDataLabels);
+    </script>
 @endpushonce
 
-@push('scripts')
 
-    <script>
-        var chart{{$graph_id}} = new Chart(
-            document.getElementById({{ $graph_id }}), @json($config)
-        );
-        Livewire.on('updateChart{{ $graph_id }}', data => {
-            // chart.data =data['data'];
-            // chart.options =data['options'];
-            // chart.config.type =data['type'];
-            // chart.update();
-            chart{{$graph_id}}.destroy()
-            chart{{$graph_id}} = new Chart(document.getElementById({{ $graph_id }}), data)
-        });
-    </script>
-@endpush
+
 
 
