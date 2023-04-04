1df569edd4a255d0ff6982c445b036a9fc9e82e4
Retype GitHub Action <retype@object.net>
2023-04-04 12:09:47 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/graph/graph.blade.php
+++ resources/views/components/graph/graph.blade.php
@@ -1,22 +1,16 @@
-<div>
-    <canvas id="{{ $graph_id }}" width="400" height="400" url="{{ $url }}"
-        type="{{ $type }}"></canvas>
-    @php
-        //  $url = url_queries(['api_token' => \Auth::user()->api_token], $url);
-    @endphp
+<div class="container">
+    <div id="{{ $graph_id }}" url="{{ $url }}" class="row">
+    </div>
 </div>
 @pushonce('scripts')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         class Graph {
             constructor(id) {
-
-                this.ctx = document.getElementById(id).getContext('2d');
+                this.id=id;
+                //this.ctx = document.getElementById(id).getContext('2d');
+                this.ctx=[];
                 this.url = document.getElementById(id).getAttribute('url');
-                // this.type = document.getElementById(id).getAttribute('type');
-
-                console.log(id, this);
-
                 this.load();
             }
 
@@ -28,8 +22,20 @@
 
             load = function() {
                 this.fetchData().then(config => {
-
-                    const myChart = new Chart(this.ctx, config);
+                    var colsize=12/config.length;
+                    for (var i = 0; i < config.length; i++) {
+                        var canvas_id=this.id+'-'+i;
+                        var canvas = $('<div class="col-'+colsize+'"><canvas width="200" height="200" id="'+canvas_id+'"   /></div>');
+                        $('#'+this.id).append(canvas);
+                        //console.log(i);
+                        //var context=canvas.getContext("2d"); 
+                        //new Chart(canvas, config);
+                        this.ctx[i] = document.getElementById(canvas_id).getContext('2d');
+                        console.log(this.ctx[i]);
+                        //new Chart(canvas, config[i]);
+                        new Chart(this.ctx[i], config[i]);
+                    }
+                    //const myChart = new Chart(this.ctx, config);
                 });
             }
 
@@ -41,4 +47,29 @@
     <script>
         new Graph('{{ $graph_id }}');
     </script>
+@endpush
+
+@push('styles')
+<style>
+    .div {
+  display: inline-block;
+}
+
+.canvas {
+  display: inline-block;
+}
+
+#canvas {
+  display: inline-block;
+}
+
+#canvas1 {
+  display: inline-block;
+}
+
+.cv {
+  display: inline-block;
+}
+
+</style>
 @endpush
