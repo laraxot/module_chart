0390595c5de4a6a2a7daca94b7da1161e6025d2f
marco76tv <marco.sottana@gmail.com>
2023-05-31 16:22:46 +0200

    up

--- Actions/JpGraph/V1/Horizbar1Action.php
+++ Actions/JpGraph/V1/Horizbar1Action.php
@@ -6,7 +6,6 @@ namespace Modules\Chart\Actions\JpGraph\V1;
 
 use Amenadiel\JpGraph\Graph\Graph;
 use Amenadiel\JpGraph\Plot\BarPlot;
-use Illuminate\Support\Str;
 use Modules\Chart\Actions\JpGraph\ApplyGraphStyleAction;
 use Modules\Chart\Actions\JpGraph\ApplyPlotStyleAction;
 use Modules\Chart\Actions\JpGraph\GetGraphAction;
@@ -20,12 +19,10 @@ class Horizbar1Action
 
     public function execute(DataCollection $answers, ChartData $chart): Graph
     {
-        // $data = $answers->toCollection()->pluck('value')->all();
         $data = $answers->toCollection()->pluck('avg')->all();
 
         $labels = $answers->toCollection()->pluck('label')->all();
-
-        // dddx(['data' => $data, 'labels' => $labels, 'chart' => $chart]);
+        /*
         $data = collect($data)->map(function ($item) {
             if (is_array($item)) {
                 return array_values($item)[0];
@@ -33,32 +30,7 @@ class Horizbar1Action
 
             return $item;
         })->all();
-
-        /*
-        $tmp = [];
-        if (null !== $chart->sublabels) {
-            dddx([$answers, $chart->sublabels]);
-            if (count($chart->sublabels) > 0) {
-                $i = 0;
-                foreach ($chart->sublabels as $k => $v) {
-                    $tmp[$i++] = collect($data)->sum($k);
-                }
-                $labels = array_values($chart->sublabels);
-                $data = $tmp;
-            }
-        }
         */
-
-        // dddx([$labels, $data]);
-        // if (isset($this->vars['names']) && \is_array($this->vars['names']) && '' !== $this->vars['group_by']) {
-        //     // dddx($this->vars['names']);
-        //     $labels = array_values($this->vars['names']); // 4 debug
-        // }
-
-        // dddx([$this->data, $this->data->pluck('label')->all(), $this->vars['names'], $this->vars['group_by']]);
-
-        // Set the basic parameters of the graph
-        // $graph = $this->getGraph();
         $graph = app(GetGraphAction::class)->execute($chart);
 
         $graph->SetScale('textlin');
@@ -68,48 +40,14 @@ class Horizbar1Action
 
         $graph = app(ApplyGraphStyleAction::class)->execute($graph, $chart);
 
-        // a true inserisce un bordo in più
-        // $graph->SetFrame(true, 'green', 5);
-
-        // a false elimina il bordo del grafico
-        // $graph->SetBox(false);
-
-        // $this->applyGraphXStyle($graph->xaxis);
-        // $this->applyGraphYStyle($graph->yaxis);
-
-        // Setup title
-        // https://jpgraph.net/download/manuals/chunkhtml/ch14s02.html
-        // dddx($this->vars['mandatory']);
-        /*
-        $mandatory = $data->mandatory;
-        if (null === $data->mandatory) {
-            $mandatory = 'null';
-        }
-        */
-        // $title = $this->vars['title'];
-        // $graph->title->Set($title);
-        // $graph->title->SetFont($this->vars['font_family'], $this->vars['font_style'], 11);
-
-        // Setup X-axis
         $graph->xaxis->SetTickLabels($labels) + 10;
 
-        // $graph->xaxis->SetLabelAngle($this->vars['style']['x_label_angle']);
-
-        // Some extra margin looks nicer
-        // $graph->xaxis->SetLabelMargin(10);
-
-        // Label align for X-axis
-        // $graph->xaxis->SetLabelAlign('right', 'center');
-
-        // Now create a bar pot
         $bplot = new BarPlot($data);
         // $bplot = $this->applyPlotStyle($bplot);
         $bplot = app(ApplyPlotStyleAction::class)->execute($bplot, $chart);
 
-        // per mettere il colore diverso alla label NR, ma qui non va bene
-        // if (Str::contains($chart->type, 'horiz')) {
         $colors = [];
-        // dddx(['data' => $data, 'labels' => $labels, 'chart' => $chart]);
+
         foreach ($labels as $k => $label) {
             if ('NR' == $label) {
                 $colors[$k] = $chart->getColors()[1].'@'.$chart->transparency;
@@ -118,7 +56,6 @@ class Horizbar1Action
             }
         }
         $bplot->SetFillColor($colors); // trasparenza, da 0 a 1
-        // }
 
         // Add the bar to the graph
         $graph->Add($bplot);
