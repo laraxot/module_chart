24801c547f3eadd51976539bfe83e6e9c32f750f
marco76tv <marco.sottana@gmail.com>
2023-07-10 22:19:39 +0200

    up

---
+++ Filament/Widgets/ChartsOverviewWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Chart\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+
+
+class ChartsOverviewWidget extends StatsOverviewWidget{
+    protected static string $view = 'chart::filament.widgets.charts-overview-widget';
+}
