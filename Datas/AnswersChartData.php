87800ea485dfa18e4294603219efb066d684ec6a
Malebestia <vair81@gmail.com>
2023-07-11 18:39:07 +0200

    .

--- Datas/AnswersChartData.php
+++ Datas/AnswersChartData.php
@@ -14,4 +14,37 @@ class AnswersChartData extends Data {
     public DataCollection $answers;
 
     public ChartData $chart;
+
+    public function getChartJsData(): array{
+        // dddx('a');
+        $setup = [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts created',
+                    // 'data' => [0, rand(1,100), 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => [5, 2],
+                    // 'data' => $data,
+                ],
+            ],
+            'labels' => ['Jan', 'Feb'],
+        ];
+
+        // dddx($setup);
+        // foreach($this->answers as $answer){
+        //     dddx($answer);
+        // }
+
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts created',
+                    // 'data' => [0, rand(1,100), 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => [5, 2],
+                    // 'data' => $data,
+                ],
+            ],
+            'labels' => ['Jan', 'Feb'],
+        ];
+    }
 }
