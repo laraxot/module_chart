94572c063953070dcf9651c9eb4359bc3c9f7d7b
marco76tv <marco.sottana@gmail.com>
2023-08-22 12:25:15 +0200

    .

---
+++ phpstan_constants.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use function Safe\define;
+
+// use Nwidart\Modules\Contracts\RepositoryInterface;
+
+// https://phpstan.org/user-guide/discovering-symbols
+
+define('LARAVEL_DIR', __DIR__);
+
+// class_alias(RepositoryInterface::class, '\Nwidart\Modules\Facades\Module');
